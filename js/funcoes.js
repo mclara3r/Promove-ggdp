bd5a666e331e475117eb5e8408ba12d676cdab8a
mclara3r <121136850+mclara3r@users.noreply.github.com>
2025-09-16 16:54:51 -0300

    dia 2 - 2

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,36 @@
-function iniciarCarreira() {
-    
+
+let mes_t = 1
+let desempenho = 1.5
+let nivel
+let dataInicio
+let pontuRema
+
+function iniciar() {
+   nivel = document.getElementById('nivel')
+   dataInicio = document.getElementById('dataInicio')
+   pontuRema = document.getElementById('pontuRema')
+   console.log(nivel)
+   console.log(dataInicio)
+   console.log(pontuRema)
 }
+
+
+function iniciarCarreira() {
+    for (let i = 0; i < 720; i++ ) {
+        carreira[i] = []
+        for (let j = 0; j <= 8; j++ ) {
+            carreira[i][0] = mes_t
+            carreira[i][1] = 0.2
+            carreira[i][2] = desempenho/6
+            carreira[i][3] = aperf/24
+            carreira[i][4] = mes_t
+            carreira[i][5] = mes_t
+            carreira[i][6] = mes_t
+            carreira[i][7] = mes_t
+            carreira[i][8] = mes_t
+            
+
+        }
+    }
+}
+
